9cf922bf7169a67011c2ded81a92bcbccae0aa6d
Sharukh Hasan <sharukh-hasan@uiowa.edu>
2016-07-18 16:36:42 -0500

    UI component fixed in splash

--- app/src/main/java/com/hypercryptic/studyaid/activity/LoginActivity.java
+++ app/src/main/java/com/hypercryptic/studyaid/activity/LoginActivity.java
@@ -3,13 +3,40 @@ package com.hypercryptic.studyaid.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.hypercryptic.studyaid.R;
 
 public class LoginActivity extends AppCompatActivity {
-
+    CallbackManager callbackManager;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
     }
 }
--- app/src/main/java/com/hypercryptic/studyaid/activity/SplashActivity.java
+++ app/src/main/java/com/hypercryptic/studyaid/activity/SplashActivity.java
@@ -12,7 +12,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_splash);
     }
